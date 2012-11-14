a4d6194cbf1b12655b74fb7125e32747bf848073
Robert Thomas <dev01@detechdev.com>
2012-11-13 16:39:33 -0800

    should work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ SimpleCms::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Added by Bob to fix the error "ActionView::Template::Error (public.css isn't precompiled"
+  config.assets.precompile += ['admin.css', 'public.css']
+
 end
